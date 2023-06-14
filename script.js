cc244f4b0b75c8fbc554506b23fae5cf2d2b95c8
Cristiano <98972381+DW-Cristiano@users.noreply.github.com>
2023-06-14 19:50:23 -0300

    XD

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    // Perform authentication and email code sending logic here
+  
+    // Redirect to a new tab after successful authentication
+    window.open("https://youtu.be/c7k7LRjMTbI", "_blank");
+  });
